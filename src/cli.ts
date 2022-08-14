9cd1c8232851d7b5a66ea56b4a0b1f7566404d3e
hardcorequal <hardcorequal@gmail.com>
2022-08-15 02:29:11 +0300

    #2: fix passing with format --rootDir  ./dirName , with space insteadof =

--- src/cli.ts
+++ src/cli.ts
@@ -1,52 +1,20 @@
 function readArgs() {
     const args = process.argv.slice(2);
 
-    // TODO: seems like it need to regenerate because format --key value not working
-
-    // filter args that not start with --
-    // const filteredArgs = args.filter(arg => arg.startsWith("--"));
-
-  //  args ( can have format --key=value  where in one argument has key and value) or (can have format --key value, where key and value are separate)
-    const { noValue, withValue }  = args.filter(arg => arg.startsWith("--")).map(arg => {
-        const [key, value] = arg.split("=");
-        return {
-            key: key.replace("--", ""),
-            value: value || true,
-        };
-    } )
-      .reduce((acc, { key, value }) => {
-        if (typeof value === 'boolean') {
-            acc.noValue.push(key);
-        } else {
-            acc.withValue.push({ key, value });
-        }
-
-        return acc;
-        // add type for initial value
-    } , { noValue: [], withValue: [] } as { noValue: string[], withValue: { key: string, value: string }[] });
-
-
-    // function for split by chunck with size
-  function chunk(arr, size) {
-    const chunked_arr = [];
-    let i = 0;
-    while (i < arr.length) {
-      chunked_arr.push(arr.slice(i, i += size));
+  const argvArr = args.reduce((acc, arg) => {
+    if (arg.includes("=")) {
+      const [key, value] = arg.split("=");
+      acc.push({ key, value });
+    } else {
+      acc.push({ key: arg, value: args[args.indexOf(arg) + 1] });
+      args.splice(args.indexOf(arg) + 1, 1);
     }
-    return chunked_arr;
-  }
+    return acc;
+  } , [] as { key: string, value: string }[])
+  // remove -- form start of key
+  .map(({ key, value }) => ({ key: key.replace(/^--/, ""), value }));
 
-    // split noValue by chunks of 2
-    const noValueChunks = chunk(noValue, 2);
-
-  // Object.fromEntries for noValueChunks
-  const noValueChunksObject = Object.fromEntries(noValueChunks);
-
-  // add noValueChunksObject to withValue
-  withValue.push(...Object.entries(noValueChunksObject).map(([key, value]) => ({ key, value })));
-
-  return new Map(withValue.map(({ key, value }) => [key, value]));
-
+  return new Map(argvArr.map(({ key, value }) => [key, value]));
 }
 
 // getRequiredArgs (by keys if exists return only required keys  else  (throw error where in error message to each missingArgs is added -- from start)) and where keys will be generic type
