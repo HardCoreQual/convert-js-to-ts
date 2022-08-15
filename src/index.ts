1ef9aeea4fbb56d309aeef41bc43ce0a3bdbdf7b
hardcorequal <hardcorequal@gmail.com>
2022-08-16 01:30:13 +0300

    #6: save empty lines in result files

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,48 @@ import {replaceModuleExportToExportDefaultTransformer, replaceRequireToImportTra
 import {getArgsByKeys} from './cli';
 
 const {rootDir, entrypoint, outputDir} = getArgsByKeys(['rootDir', 'entrypoint'], ['outputDir']);
-const codeDir = path.resolve(process.cwd(), rootDir);
+const codeDir = path.relative(process.cwd(), rootDir);
 
-const program = ts.createProgram([path.join(rootDir, entrypoint)], { allowJs: true });
+const a = fs.existsSync(path.join(rootDir, entrypoint));
 
-const files = program.getSourceFiles();
+let program = ts.createProgram([path.join(rootDir, entrypoint)], { allowJs: true });
 
-const projectFiles = files
+let files = program.getSourceFiles();
+
+let projectFiles = files
+  .filter(file => !file.fileName.includes('node_modules'))
   .filter(file => {
     return path.resolve(file.fileName).includes(path.resolve(codeDir));
+  } );
+
+projectFiles.forEach(file => {
+  const fileContent = fs.readFileSync(file.fileName, 'utf8');
+  const newFileContent = fileContent.split('\n').map(line => {
+    if (line.trim() === '') {
+      return '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C';
+    }
+    return line;
+  } ).join('\n');
+
+  const relativePath = path.relative(rootDir, file.fileName);
+
+  const outputFileName = path.join(outputDir, relativePath);
+
+  const outputDirName = path.dirname(outputFileName);
+  if (!fs.existsSync(outputDirName)) {
+    fs.mkdirSync(outputDirName, { recursive: true });
+  }
+
+  fs.writeFileSync(outputFileName, newFileContent);
+} );
+
+program = ts.createProgram([path.join(outputDir, entrypoint)], { allowJs: true });
+files = program.getSourceFiles();
+
+projectFiles = files
+  .filter(file => !file.fileName.includes('node_modules'))
+  .filter(file => {
+    return path.resolve(file.fileName).includes(path.resolve(outputDir));
   } );
 
 const result = ts.transform<ts.SourceFile>(projectFiles, [
@@ -25,14 +58,14 @@ const printer = ts.createPrinter();
 
 
 for (const file of result.transformed) {
-  const filePath = file.fileName;
-  const filePathRelative = path.relative(process.cwd(), filePath);
+  const fileContent = printer.printFile(file);
 
-  const fullDirName = path.join(outputDir ?? '.', path.dirname(filePathRelative));
+  const newFileContent = fileContent.split('\n').map(line => {
+    if (line.trim() === '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C') {
+      return '';
+    }
+    return line;
+  } ).join('\n');
 
-  if (!fs.existsSync(fullDirName)) {
-    fs.mkdirSync(fullDirName, { recursive: true });
-  }
-
-  fs.writeFileSync(path.resolve(path.join(fullDirName, path.basename(filePathRelative))), printer.printFile(file));
+  fs.writeFileSync(file.fileName, newFileContent);
 }
