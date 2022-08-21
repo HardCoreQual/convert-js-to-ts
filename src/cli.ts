3c6a2c026d8d6b259268ed6f7551c6c035c9b886
hardcorequal <hardcorequal@gmail.com>
2022-08-21 14:49:18 +0300

    #17: use `commander` like cli framework

--- src/cli.ts
+++ src/cli.ts
@@ -1,41 +1,18 @@
-function readArgs() {
-    const args = process.argv.slice(2);
+import {ConversionParams, index} from './index';
+import { Command } from 'commander';
+import { version, description, name } from '../package.json';
 
-  const argvArr = args.reduce((acc, arg) => {
-    if (arg.includes("=")) {
-      const [key, value] = arg.split("=");
-      acc.push({ key, value });
-    } else {
-      acc.push({ key: arg, value: args[args.indexOf(arg) + 1] });
-      args.splice(args.indexOf(arg) + 1, 1);
-    }
-    return acc;
-  } , [] as { key: string, value: string }[])
-  // remove -- form start of key
-  .map(({ key, value }) => ({ key: key.replace(/^--/, ""), value }));
+const program = new Command();
 
-  return new Map(argvArr.map(({ key, value }) => [key, value]));
-}
+program
+  .name(name)
+  .description(description)
+  .version(version)
+  .option('-p, --projectDir <string>', 'path/to/project')
+  .option('-e, --entrypoint <string>', 'path/to/entrypoint/in/project')
+  .option('-o, --outputDir <string>', 'path/to/output/dir', '.')
+  .action((options: ConversionParams) => {
+    index(options);
+  });
 
-export function getArgsByKeys<T extends string, O extends string>(keys: T[], optionalKeys?: O[]): { [K in T]: string } & { [K in O]?: string } {
-    const args = readArgs();
-    const missingArgs = keys.filter(key => !args.has(key));
-    if (missingArgs.length > 0) {
-      //  trow error about missing args  where each argument have format --key=value
-        throw new Error(`Missing arguments: ${missingArgs.map(key => `--${key}=value`).join(" ")}`);
-    }
-
-    const requiredArgs = keys.reduce((acc, key) => {
-        // @ts-ignore
-      acc[key] = args.get(key);
-        return acc;
-    } , {} as {[key in T]: string});
-
-    const optionalArgs = optionalKeys?.reduce((acc, key) => {
-        acc[key] = args.get(key);
-        return acc;
-    } , {} as {[key in O]: string | undefined});
-
-    // @ts-ignore
-  return { ...requiredArgs, ...optionalArgs };
-}
+program.parse();
--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,131 @@
-import * as ts from "typescript";
-import * as fs from "fs";
-import * as path from 'path';
-import {replaceModuleExportToExportDefaultTransformer, replaceRequireToImportTransformer } from './transformers';
-import {getArgsByKeys} from './cli';
-
 import util from 'util';
 import child_process from 'child_process';
+import path from 'path';
+import process from 'process';
+import fs from 'fs';
+import * as ts from 'typescript';
+import {replaceModuleExportToExportDefaultTransformer, replaceRequireToImportTransformer} from './transformers';
 
 const exec = util.promisify(child_process.exec);
-
-
-const {projectDir, entrypoint, outputDir} = getArgsByKeys(['projectDir', 'entrypoint'], ['outputDir']);
-const codeDir = path.relative(process.cwd(), projectDir);
-
-let entrypointPath = path.resolve(path.join(codeDir, entrypoint));
-
-if(!fs.existsSync(entrypointPath)) {
-  throw new Error(`Entrypoint ${entrypointPath} does not exist`);
+export type ConversionParams = {
+  projectDir: string;
+  outputDir: string;
+  entrypoint: string;
 }
 
-let program = ts.createProgram([entrypointPath], { allowJs: true });
+export function index({projectDir, outputDir, entrypoint}: ConversionParams) {
+  const codeDir = path.relative(process.cwd(), projectDir);
 
-let files = program.getSourceFiles();
+  let entrypointPath = path.resolve(path.join(codeDir, entrypoint));
 
-let projectFiles = files
-  .filter(file => !file.fileName.includes('node_modules') && file.fileName.endsWith('.js'))
-  .filter(file => {
-    return path.resolve(file.fileName).includes(path.resolve(codeDir));
-  } );
-
-if(projectFiles.length === 0) {
-  throw new Error(`No project files found in ${codeDir}, by entrypoint ${entrypointPath}`);
-}
-
-const emptyLineComment = '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C';
-
-projectFiles.forEach(file => {
-  const fileContent = fs.readFileSync(file.fileName, 'utf8');
-  const newFileContentArr = fileContent.split('\n');
-
-  const newFileContent = newFileContentArr.map(line => {
-    if (line.trim() === '') {
-      return emptyLineComment;
-    }
-    return line;
-  } ).join('\n');
-
-  const relativePath = path.relative(projectDir, file.fileName);
-
-  const outputFileName = path.join(outputDir ?? projectDir, relativePath);
-
-  const outputDirName = path.dirname(outputFileName);
-  if (!fs.existsSync(outputDirName)) {
-    fs.mkdirSync(outputDirName, { recursive: true });
+  if (!fs.existsSync(entrypointPath)) {
+    throw new Error(`Entrypoint ${entrypointPath} does not exist`);
   }
 
-  fs.writeFileSync(outputFileName, newFileContent);
-} );
+  let program = ts.createProgram([entrypointPath], {allowJs: true});
 
-entrypointPath = path.resolve(path.join(outputDir ?? projectDir, entrypoint));
+  let files = program.getSourceFiles();
 
-if(!fs.existsSync(entrypointPath)) {
-  throw new Error(`Converted Entrypoint ${entrypointPath} does not exist`);
-}
-
-program = ts.createProgram([entrypointPath], { allowJs: true });
-files = program.getSourceFiles();
-
-projectFiles = files
-  .filter(file => !file.fileName.includes('node_modules') && file.fileName.endsWith('.js'))
-  .filter(file => {
-    return path.resolve(file.fileName).includes(path.resolve(outputDir ?? codeDir));
-  } );
-
-if(projectFiles.length === 0) {
-  throw new Error(`Not found converted project files found in ${outputDir}, by entrypoint ${entrypointPath}`);
-}
-
-
-const result = ts.transform<ts.SourceFile>(projectFiles, [
-  replaceModuleExportToExportDefaultTransformer,
-  replaceRequireToImportTransformer,
-])
-
-const printer = ts.createPrinter();
-
-if (result.transformed.length === 0) {
-  throw new Error(`No transformed files found`);
-}
-
-Promise.all(result.transformed.map(async (file) => {
-  const fileContent = printer.printFile(file);
-
-  const newFileContent = fileContent.split('\n').map(line => {
-    if (line.trim() === '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C') {
-      return '';
-    }
-    return line;
-  } ).join('\n');
-
-  const directoryPath = path.dirname(file.fileName);
-  const basename = path.basename(file.fileName);
-
-  const fullBaseName = path.join(directoryPath, basename);
-
-  fs.writeFileSync(fullBaseName, newFileContent);
-
-  if (fullBaseName.endsWith('.js')) {
-    const newFullFileName = fullBaseName.slice(0, -'js'.length) + 'ts';
-    const gitMvCommand = `git mv ${fullBaseName} ${newFullFileName}`;
-    await exec(gitMvCommand, { cwd: projectDir });
-
-    return newFullFileName;
-  }
-
-  return fullBaseName;
-}))
-  .then(files => {
-    const eslintCommand = `npx eslint --fix ${files.join(' ')}`;
-    return exec(eslintCommand, { cwd: projectDir }).catch((e) => {
-      console.log( e )
+  let projectFiles = files
+    .filter(file => !file.fileName.includes('node_modules') && file.fileName.endsWith('.js'))
+    .filter(file => {
+      return path.resolve(file.fileName).includes(path.resolve(codeDir));
     });
-  } )
-  .then(() => {
-    console.log( 'Project is convert to TypeScript' );
-})
+
+  if (projectFiles.length === 0) {
+    throw new Error(`No project files found in ${codeDir}, by entrypoint ${entrypointPath}`);
+  }
+
+  const emptyLineComment = '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C';
+
+  projectFiles.forEach(file => {
+    const fileContent = fs.readFileSync(file.fileName, 'utf8');
+    const newFileContentArr = fileContent.split('\n');
+
+    const newFileContent = newFileContentArr.map(line => {
+      if (line.trim() === '') {
+        return emptyLineComment;
+      }
+      return line;
+    }).join('\n');
+
+    const relativePath = path.relative(projectDir, file.fileName);
+
+    const outputFileName = path.join(outputDir ?? projectDir, relativePath);
+
+    const outputDirName = path.dirname(outputFileName);
+    if (!fs.existsSync(outputDirName)) {
+      fs.mkdirSync(outputDirName, {recursive: true});
+    }
+
+    fs.writeFileSync(outputFileName, newFileContent);
+  });
+
+  entrypointPath = path.resolve(path.join(outputDir ?? projectDir, entrypoint));
+
+  if (!fs.existsSync(entrypointPath)) {
+    throw new Error(`Converted Entrypoint ${entrypointPath} does not exist`);
+  }
+
+  program = ts.createProgram([entrypointPath], {allowJs: true});
+  files = program.getSourceFiles();
+
+  projectFiles = files
+    .filter(file => !file.fileName.includes('node_modules') && file.fileName.endsWith('.js'))
+    .filter(file => {
+      return path.resolve(file.fileName).includes(path.resolve(outputDir ?? codeDir));
+    });
+
+  if (projectFiles.length === 0) {
+    throw new Error(`Not found converted project files found in ${outputDir}, by entrypoint ${entrypointPath}`);
+  }
+
+
+  const result = ts.transform<ts.SourceFile>(projectFiles, [
+    replaceModuleExportToExportDefaultTransformer,
+    replaceRequireToImportTransformer,
+  ])
+
+  const printer = ts.createPrinter();
+
+  if (result.transformed.length === 0) {
+    throw new Error(`No transformed files found`);
+  }
+
+  Promise.all(result.transformed.map(async (file) => {
+    const fileContent = printer.printFile(file);
+
+    const newFileContent = fileContent.split('\n').map(line => {
+      if (line.trim() === '//  41waZZbs86CZpAGqn2cYnQreZrGaduFZkKFyZp3TNsj55zsINjmpIPxnohl7ZXCWBOVdYw4w9SV6776D3Ro0WXrmObkuQMKsGjlTOLAI32YAmKIje4X8vIovvkAPA60C') {
+        return '';
+      }
+      return line;
+    }).join('\n');
+
+    const directoryPath = path.dirname(file.fileName);
+    const basename = path.basename(file.fileName);
+
+    const fullBaseName = path.join(directoryPath, basename);
+
+    fs.writeFileSync(fullBaseName, newFileContent);
+
+    if (fullBaseName.endsWith('.js')) {
+      const newFullFileName = fullBaseName.slice(0, -'js'.length) + 'ts';
+      const gitMvCommand = `git mv ${fullBaseName} ${newFullFileName}`;
+      await exec(gitMvCommand, {cwd: projectDir});
+
+      return newFullFileName;
+    }
+
+    return fullBaseName;
+  }))
+    .then(files => {
+      const eslintCommand = `npx eslint --fix ${files.join(' ')}`;
+      return exec(eslintCommand, {cwd: projectDir}).catch((e) => {
+        console.log(e)
+      });
+    })
+    .then(() => {
+      console.log('Project is convert to TypeScript');
+    });
+}
